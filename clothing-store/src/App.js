83675716fe6d65f85f4febf0784943aaa1172f53
Gisman4ik <Sftawitkakpro@gmail.com>
2020-07-06 21:54:31 +0300

    feat: add main routes

--- clothing-store/src/App.js
+++ clothing-store/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter,Switch,Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default function App() {
+    return (
+      <BrowserRouter>
+        <div className="app-wrapper">
+            <Header />
+            <Navbar />
+            <div className="app-content-wrapper">
+                    <Switch>
+                    <Route path="/" component={Home}>
+                        <Home />
+                    </Route>
+                    <Route path="/catalog" component={Catalog}>
+                        <Catalog />
+                    </Route>
+                    <Route path="/contacts" component={Contacts}>
+                        <Contacts />
+                    </Route>
+                    <Route path="/other" component={Other}>
+                        <Other />
+                    </Route>
+                    <Route path="/cart" component={Cart}>
+                        <Cart />
+                    </Route>
+                </Switch>
+            </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
