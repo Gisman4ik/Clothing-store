78fd37bb01e0bbe5599c5db15355f04c65736831
Gisman4ik <Sftawitkakpro@gmail.com>
2020-07-07 17:35:28 +0300

    refactor: add Layout

--- clothing-store/src/App.js
+++ clothing-store/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { BrowserRouter,Switch,Route, Link } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout'
 import Home from './components/Home/Home';
 import Catalog from './components/Catalog/Catalog';
 import Contacts from './components/Contacts/Contacts';
@@ -12,27 +11,27 @@ export default function App() {
     return (
       <BrowserRouter>
         <div className="app-wrapper">
-            <Header />
-            <Navbar />
-            <div className="app-content-wrapper">
-                <Switch>
-                    <Route path="/" component={Home}>
-                        <Home/>
-                    </Route>
-                    <Route path="/catalog" component={Catalog}>
-                        <Catalog />
-                    </Route>
-                    <Route path="/contacts" component={Contacts}>
-                        <Contacts />
-                    </Route>
-                    <Route path="/other" component={Other}>
-                        <Other />
-                    </Route>
-                    <Route path="/cart" component={Cart}>
-                        <Cart />
-                    </Route>
-                </Switch>
-            </div>
+            <Layout>
+                <div className="app-content-wrapper">
+                    <Switch>
+                        <Route exact path="/" component={Home}>
+                            <Home/>
+                        </Route>
+                        <Route path="/catalog" component={Catalog}>
+                            <Catalog />
+                        </Route>
+                        <Route path="/contacts" component={Contacts}>
+                            <Contacts />
+                        </Route>
+                        <Route path="/other" component={Other}>
+                            <Other />
+                        </Route>
+                        <Route path="/cart" component={Cart}>
+                            <Cart />
+                        </Route>
+                    </Switch>
+                </div>
+            </Layout>
         </div>
       </BrowserRouter>
     );
---
+++ clothing-store/src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import React, {Component} from 'react'
+
+export default function Layout () {
+    return (
+        <div>
+            <Header />
+            {}
+            <Footer />
+        </div>
+    )
+}
